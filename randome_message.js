2ba6579bdad7360638ce72864a5f3b8cf3ae1dc3
davesetsfire <dpeichl@gmail.com>
2023-07-11 15:01:36 +0200

    First Commit

---
+++ randome_message.js
@@ -0,0 +1,23 @@
+let listPerson = ["You", 'Your'];
+let subject = ["day", "life", "relationship"];
+let adjective = ["happy", "boring", "cool", "funny", "lucky"];
+
+function createMessage() {
+    let randomeNumberOne = Math.floor(Math.random()*2);
+    if (randomeNumberOne === 0) {
+        return listPerson[0] + " are " + adjective[Math.floor(Math.random()*adjective.length)];
+    } else {
+        return listPerson[1] + ' ' + subject[Math.floor(Math.random())*subject.length] + ' will be ' + adjective[Math.floor(Math.random()*adjective.length)]
+    }
+    
+}
+console.log(createMessage());
+
+
+
+
+
+
+
+
+
